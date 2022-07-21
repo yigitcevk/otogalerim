4f9e4473ccf8d262500dea5b994c9cd67d82d235
TahsinEnesKuru <teneskuru@icloud.com>
2022-07-21 22:01:57 +0300

    ui is implemented

---
+++ ui/src/App.vue
@@ -0,0 +1,465 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+
+<script>
+export default {
+}
+</script>
+
+
+<style>
+
+body {
+  font-family: "Public Sans";
+}
+
+.button-white {
+  cursor: pointer;
+  float: right;
+  padding: 14px 24px;
+  background: #ffffff;
+  color: #0d1821;
+  text-decoration: none;
+  font-weight: bold;
+  border: 2px solid #0d1821;
+  box-sizing: border-box;
+  border-radius: 40px;
+}
+
+.button-white:hover{
+  box-shadow: 0px 0px 20px black;
+  transition: box-shadow 0.2s ease-in-out;  
+}
+
+.button-black:hover{
+  box-shadow: 0px 0px 20px black;
+  transition: box-shadow 0.2s ease-in-out;  
+}
+
+.button-see-more {
+  float: none;
+  display: inline-block;
+  font-weight: bold;
+  font-size: 16px;
+}
+
+.button-white > .text {
+  display: inline-block;
+  font-size: 16px;
+  vertical-align: middle;
+  margin-right: 8px;
+}
+
+.button-white > .material-icons {
+  display: inline-block;
+  font-size: 20px;
+  vertical-align: middle;
+}
+
+@media only screen and (max-width: 700px) {
+  .button-white > .text {
+    display: none;
+  }
+  .button-white {
+    padding: 14px 14px;
+  }
+  .button-black {
+    display: none;
+  }
+}
+
+.button-black {
+  cursor: pointer;
+  float: right;
+  margin-left: 24px;
+  /*width: 161px;*/
+  /*height: 48px;*/
+  padding: 16px 24px;
+  background: #0d1821;
+  color: #ffffff;
+  text-decoration: none;
+  font-weight: bold;
+  border: 2px solid #0d1821;
+  box-sizing: border-box;
+  border-radius: 40px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+}
+
+.button-black > .text {
+  display: inline-block;
+  font-size: 16px;
+  vertical-align: middle;
+}
+
+.icon {
+  vertical-align: middle;
+  float: left;
+  /*margin-right: 10%;*/
+}
+
+.navbar {
+  /*padding-left: 48px;
+      padding-right: 48px;
+      padding-top: 47px;*/
+  display: block;
+  position: static;
+  /* background: red; */
+}
+
+.search-bar {
+  background: blue;
+}
+
+/*Eski CSS:*/
+
+a:link {
+  text-decoration: none;
+}
+
+a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+a:active {
+  text-decoration: none;
+}
+
+body {
+  padding: 0px;
+  margin: 0px;
+  font-family: Tahoma, sans-serif;
+}
+
+.top-frame {
+  padding: 16px;
+  background: #dced31;
+  padding-bottom: 20px;
+  border-bottom: 1px solid black;
+}
+
+.middle-frame {
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  padding-bottom: 20px;
+  padding-left: 18%;
+  padding-right: 18%;
+}
+
+.answer-frame {
+  padding:16px;
+  max-width: 664px;
+  min-height: 400px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.bottom-frame {
+  padding:16px;
+  background: #0d1821;
+  padding-bottom: 20px;
+  padding-left: 5%;
+  padding-top: 5.5%;
+}
+
+.navbar {
+  padding: 47px;
+}
+
+.logo {
+}
+
+.button {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 14px 24px;
+  position: static;
+  width: 191px;
+  height: 48px;
+  left: 0px;
+  top: 0px;
+  /* Special / White */
+  background: #ffffff;
+  /* Grey / 500 - #0D1821 */
+  border: 2px solid #0d1821;
+  box-sizing: border-box;
+  border-radius: 40px;
+  /* Inside Auto Layout */
+  font-family: Tahoma, sans-serif;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin: 0px 24px;
+}
+
+.elipse {
+  /* position: absolute; */
+  width: 64px;
+  height: 64px;
+  border: 2px solid #dced31;
+  /* box-sizing: border-box; */
+  border-radius: 50%;
+  align-items: center;
+  display: flex;
+}
+
+.bottom-link {
+  color: #dced31;
+  font-weight: 600;
+  font-size: 19px;
+  line-height: 140%;
+}
+
+.form {
+  /* This bit sets up the horizontal layout */
+  display: flex;
+  flex-direction: row;
+  /* This bit draws the box around it */
+  background: #ffffff;
+  /* Shadow / 1 */
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.05), 0px 25px 35px rgba(0, 0, 0, 0.03);
+  border-radius: 8px;
+  max-width: 664px;
+  width: 100%;
+  height: 56px;
+  /* I've used padding so you can see the edges of the elements. */
+  padding: 2px;
+  /* padding-left: 16px;
+  padding-right: 16px; */
+  margin:8px;
+
+}
+
+.form:hover{
+  box-shadow: 0px 0px 20px gray;
+  transition: box-shadow 0.2s ease-in-out;
+}
+
+.form:focus-within{
+  box-shadow: 0px 0px 20px gray;
+  transition: box-shadow 0.2s ease-in-out;
+}
+
+input{
+  padding-left:16px;
+  padding-left:16px;
+}
+
+input,
+input:placeholder-shown {
+  /* Special / White */
+  /* Tell the input to use all the available space */
+  flex-grow: 2;
+  /* And hide the input's outline, so the form looks like the outline */
+  border: none;
+  font-size: 16px;
+  line-height: 140%;
+  /* or 22px */
+  letter-spacing: -0.2px;
+}
+
+input:focus {
+  outline: none !important;
+
+}
+
+button {
+  /* Just a little styling to make it pretty */
+  /* border:1px solid blue; */
+  background: #ffffff;
+  /* color:white; */
+  border: none;
+  cursor: pointer;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  text-align: center;
+  align-items: center;
+  margin-top: 50px;
+}
+
+div.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(16, 16, 16, 0.5);
+}
+
+@-webkit-keyframes uil-ring-anim {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes uil-ring-anim {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-moz-keyframes uil-ring-anim {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-ms-keyframes uil-ring-anim {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-moz-keyframes uil-ring-anim {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes uil-ring-anim {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-o-keyframes uil-ring-anim {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes uil-ring-anim {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+.uil-ring-css {
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 200px;
+  height: 200px;
+}
+.uil-ring-css > div {
+  position: absolute;
+  display: block;
+  width: 160px;
+  height: 160px;
+  top: 20px;
+  left: 20px;
+  border-radius: 80px;
+  box-shadow: 0 6px 0 0 #ffffff;
+  -ms-animation: uil-ring-anim 1s linear infinite;
+  -moz-animation: uil-ring-anim 1s linear infinite;
+  -webkit-animation: uil-ring-anim 1s linear infinite;
+  -o-animation: uil-ring-anim 1s linear infinite;
+  animation: uil-ring-anim 1s linear infinite;
+}
+
+.container-row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
---
+++ ui/src/components/SearchCar.vue
@@ -0,0 +1,240 @@
+<template>
+<div class="top-frame">
+
+
+  <div class="container" style="text-align: center; align-items: center">
+    <img
+      src="@/assets/iconlarge.png"
+      v-bind:style="[
+        curQuery ? 'width:100px;height:100px;float:left;' : '',
+      ]"
+    />
+
+  <div class = 'brandCards'>
+      <div>
+        <label for="brand">
+          Select Brand
+        </label>
+        <select :placeholder="[[ asd ]]" class='selectType' name="brand" @change="brandClicked($event)" >
+          <option v-for="item in brands" :key="item.id" :value="item.id">
+              {{item.name}}
+          </option>
+        </select>
+      </div>
+
+      <div>
+        <label for="brand">
+          Select Model
+        </label>
+      
+        <select v-if="models!=null" class = 'selectType' name="model" @change="modelClicked($event)">
+            <option v-for="item in models[brandId]" :key="item.id" :value="item.id">
+                {{item.name}}
+            </option>
+        </select>
+      </div>
+
+      <a class="button-add" v-on:click="newCar()">
+          Add
+      </a>
+
+  </div>
+
+
+  <div class="form">
+    <div class='addedCars'>
+      <div v-for="item in addedCars" :key="item.modelId">
+          <a class='button-white' @click="removeCar(item)">
+            {{brands[item.brandId].name}} {{models[item.brandId][item.modelId].name}} 
+          </a>
+      </div>
+    </div>
+  </div>
+
+  <div style='width:200px'>
+    <a class="button-add" v-on:click="search()">
+        Search
+    </a>
+
+  </div>  
+
+    <h1>
+      Save time for buying cars!
+    </h1>
+    <p>
+      We will search the autogaleries for your dream car.
+    </p>
+  </div>
+
+
+
+</div>
+
+
+</template>
+
+
+<script>
+export default {
+  name: 'SearchCar',
+  components: {
+  },
+  data() {
+    return {
+      key:"",
+      query: "",
+      brandId:0,
+      modelId:null,
+      addedCars:[],
+      brands:null,
+      models:null,      
+    }
+  },
+  watch: {
+    curQuery(newVal) {
+      this.query = newVal;
+    }
+  },
+  created() {
+    this.fetchBrands();
+    this.fetchModels();
+
+  },
+  methods: {
+    fetchBrands() {
+      fetch("http://127.0.0.1:5000/brands")
+        .then(response => {
+          console.log(response.json());
+          this.brands = response.json();
+        })
+        .catch(error => {
+          console.error('Panic at the disco:', error);
+        })
+    },
+    fetchModels() {
+      fetch("http://127.0.0.1:5000/models")
+        .then(response => {
+          this.models = response.json();
+        })
+        .catch(error => {
+          console.error('Panic at the disco:', error);
+        })
+    },    
+    brandClicked(e) {
+      this.brandId = e.target.value;
+    },
+    modelClicked(e){
+      this.modelId = e.target.value;
+    },
+    newCar() {
+      let car = this.addedCars.find(car => car.brandId === this.brandId && car.modelId === this.modelId)
+      if (car == null)   
+        this.addedCars.push({"brandId":this.brandId,"modelId":this.modelId})
+      else
+        alert("this car is already added");
+    },
+    removeCar(item) {
+      this.addedCars = this.addedCars.filter(data => (data.modelId != item.modelId || data.brandId != item.brandId));
+    }
+  }
+}
+
+</script>
+
+
+<style>
+
+.form {
+  display: flex;
+  flex-direction: row;
+  background: #ffffff;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.05), 0px 25px 35px rgba(0, 0, 0, 0.03);
+  border-radius: 8px;
+  max-width: 664px;
+  width: 100%;
+  height: 56px;
+  padding: 2px;
+  margin:8px;
+
+}
+
+.form:hover{
+  box-shadow: 0px 0px 20px gray;
+  transition: box-shadow 0.2s ease-in-out;
+}
+
+.form:focus-within{
+  box-shadow: 0px 0px 20px gray;
+  transition: box-shadow 0.2s ease-in-out;
+}
+
+.button-added{
+  cursor: pointer;
+  float: right;
+  padding: 14px 24px;
+  margin:8px;
+  background: #ffffff;
+  color: #0d1821;
+  text-decoration: none;
+  font-weight: bold;
+  border: 2px solid #0d1821;
+  box-sizing: border-box;
+  border-radius: 40px;  
+}
+
+.addedCars{
+  display:flex;
+  flex-direction:row;
+
+}
+
+.selectType{
+
+  width:160px
+}
+
+
+.button-add:hover{
+  box-shadow: 0px 0px 20px black;
+  transition: box-shadow 0.2s ease-in-out;  
+}
+
+.button-add {
+  cursor: pointer;
+  float: right;
+  padding: 8px 12px;
+  margin-top:5px;
+  margin-bottom:5px;
+  background: #0d1821;
+  color: #ffffff;
+  width:50%;
+  text-decoration: none;
+  font-weight: bold;
+  border: 2px solid #0d1821;
+  box-sizing: border-box;
+  border-radius: 40px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+}
+
+
+.button-add > .text {
+  font-size: 8px;
+  vertical-align: middle;
+}
+
+.brandCards{
+
+  display: flex;
+  flex-direction: column;
+  text-align:center;
+  align-items: center;
+  /* This bit draws the box around it */
+  background: #ffffff;
+  /* Shadow / 1 */
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.05), 0px 25px 35px rgba(0, 0, 0, 0.03);
+  border-radius: 8px;
+  width:20%;
+}
+
+
+</style>
