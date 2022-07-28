774f34e6603de91809b30b4210422ae537a7f189
TahsinEnesKuru <teneskuru@icloud.com>
2022-07-28 20:40:54 +0300

    new car enter is added

--- backend/app/controller/api_controller.py
+++ backend/app/controller/api_controller.py
@@ -120,4 +120,42 @@ def placeCar():
     conn.commit()
 
     return jsonify({"message":"basariyla silindi"})
+
+@controller.route('/enterCar', methods=['post'])
+def enterCar():
+    print(request.json)
+    if request.json is not None:
+        galleryId = request.json['gallery_id']
+        km = request.json['km']
+        motor_hacmi = request.json['motor_hacmi']
+        motor_gucu = request.json['motor_gucu']
+        vites = request.json['vites']
+        fue = request.json['fue']
+        state = request.json['state']
+        color = request.json['color']
+        brand_id = request.json['brand_id']
+        model_id = request.json['model_id']
+        uretim_yili = request.json['uretim_yili']
+        print(galleryId,km,motor_hacmi,motor_gucu,vites,fue,state,color,brand_id,model_id)
+    else:
+        return 'gallery_id, car_id and price must be defined', 400
+
+    conn = psycopg2.connect(database = "otogaleri", user = "postgres", password = "enes1324", host = "127.0.0.1", port = "5432")
+    cur = conn.cursor()
+
+    # try:
+    on_sale=False
+    car_id = randint(1,999999)
+
+    sql_select_query = """INSERT INTO car values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
+    cur.execute(sql_select_query, (car_id,model_id,brand_id,color,km,state,fue,vites,motor_hacmi,uretim_yili,motor_gucu,galleryId,))
+
+    sql_select_query = """INSERT INTO own values (%s,%s,%s);"""
+    cur.execute(sql_select_query, (car_id,galleryId,on_sale,))
+
+    # except:
+    #     return jsonify({"message":"bir hata olustu"})
+    conn.commit()
+
+    return jsonify({"message":"basariyla silindi"})    
       
--- ui/src/views/GalleryAdmin.vue
+++ ui/src/views/GalleryAdmin.vue
@@ -32,7 +32,104 @@
 
     </div>
 
+    <h1>
+       Enter your new car to system!!
+    </h1>    
 
+
+  <div class = 'brandCards'>
+      <div>
+        <label for="brand">
+          Select Brand
+        </label>
+        <select class='selectType' name="brand" @change="brandClicked($event)" >
+          <option v-for="item in brands" :key="item.id" :value="item.id">
+              {{item.name}}
+          </option>
+        </select>
+      </div>
+
+      <div>
+        <label for="brand">
+          Select Model
+        </label>
+      
+        <select v-if="models!=null" class = 'selectType' name="model" @change="modelClicked($event)">
+            <option v-for="item in models[brandId]" :key="item.id" :value="item.id">
+                {{item.name}}
+            </option>
+        </select>
+      </div>
+
+
+      <div>
+        <label for="brand">
+          Select Color
+        </label>
+      
+        <select class = 'selectType' name="model" @change="colorClicked($event)">
+            <option v-for="item in color" :key="item.id" :value="item.name">
+                {{item.name}}
+            </option>
+        </select>
+      </div>
+
+      <div>
+        <label for="brand">
+          Select State
+        </label>
+      
+        <select class = 'selectType' name="model" @change="stateClicked($event)">
+            <option v-for="item in state" :key="item.id" :value="item.name">
+                {{item.name}}
+            </option>
+        </select>
+      </div>  
+
+        <div>
+        <label for="brand">
+          Select Fue
+        </label>
+      
+        <select class = 'selectType' name="model" @change="fueClicked($event)">
+            <option v-for="item in fue" :key="item.id" :value="item.name">
+                {{item.name}}
+            </option>
+        </select>
+      </div> 
+
+         <div>
+        <label for="brand">
+          Select Vites
+        </label>
+      
+        <select class = 'selectType' name="model" @change="vitesClicked($event)">
+            <option v-for="item in vites" :key="item.id" :value="item.name">
+                {{item.name}}
+            </option>
+        </select>
+      </div>       
+
+
+    <div>
+        <label for="brand">
+          Select Volume
+        </label>
+      
+        <select class = 'selectType' name="model" @change="hacimClicked($event)">
+            <option v-for="item in motor_hacmi" :key="item.id" :value="item.name">
+                {{item.name}}
+            </option>
+        </select>
+      </div>
+
+    <input class="email-input" style="border-style: solid; margin-top: 5px;" v-model="hp" placeholder="Enter HP" />
+    <input class="email-input" style="border-style: solid; margin-top: 5px;" v-model="km" placeholder="Enter KM" />
+    <input class="email-input" style="border-style: solid; margin-top: 5px;" v-model="uretim_yili" placeholder="Enter Production Year" />
+    <a style='width:80px' class="button-login" v-on:click="enter()">
+        Enter 
+    </a>
+  </div>
 
   </div>
 </template>
@@ -46,11 +143,28 @@ export default {
     },
     data(){
         return{
+            brandId:0,
+            modelId:null,            
             galleryId:'',
             forSaleCars:[],
             noSaleCars:[],
             price:null,
-            currentCar:null
+            km:null,
+            hp:null,
+            currentCar:null,
+            brands:null,
+            models:null,
+            uretim_yili:null,
+            currentColor:'Siyah',
+            currentState:'2.el',
+            currentFue:'Benzin',
+            currentVites:'Manuel',
+            currentMotorHacmi:'1.3',
+            color : [{'id':'1','name':'Siyah'}, {'id':'2','name':'Gri'},{'id':'3','name':'Kırmızı'}, {'id':'4','name':'Mavi'}, {'id':'5','name':'Lacivert'}, {'id':'6','name':'Beyaz'},{'id':'7','name':'Yeşil'},{'id':'8','name':'Sarı'}],
+            state : [{'id':'1','name':'2.el'}, {'id':'2','name':'Sıfır'}],
+            fue : [{'id':'1','name':'Benzin'}, {'id':'1','name':'Dizel'}, {'id':'1','name':'LPG'}, {'id':'1','name':'Elektrik'}, {'id':'1','name':'Benzin & LPG'}],
+            vites : [{'id':'1','name':'Manuel'},{'id':'2','name':'Yarı Otomatik'}, {'id':'3','name':'Otomatik'}],
+            motor_hacmi : [{'id':'1','name':'1.3'}, {'id':'2','name':'1.4'}, {'id':'3','name':'1.5'}, {'id':'4','name':'1.6'}]            
         }
     },
     created() {
@@ -92,6 +206,37 @@ export default {
             console.error("There was an error!", error);
         });
 
+        fetch("http://127.0.0.1:5000/brands")
+        .then(async response => {
+            const data = await response.json();
+
+            if (!response.ok) {
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+            }
+            this.brands = data;
+        })
+        .catch(error => {
+            this.errorMessage = error;
+            console.error("There was an error!", error);
+        });
+
+        fetch("http://127.0.0.1:5000/models")
+        .then(async response => {
+            const data = await response.json();
+
+            if (!response.ok) {
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+            }
+
+            this.models = data;
+        })
+        .catch(error => {
+            this.errorMessage = error;
+            console.error("There was an error!", error);
+        });        
+
     },
     methods: {
         removeCarFromSale(car_id) {
@@ -113,10 +258,32 @@ export default {
             });
             location.reload();
         },
+        brandClicked(e) {
+            this.brandId = e.target.value;
+            this.modelId = this.models[this.brandId][0].id;
+        },
+        modelClicked(e){
+            this.modelId = e.target.value;
+        },        
         carClicked(e){
             this.currentCar = e.target.value;
         },
-        place() {
+        colorClicked(e){
+            this.currentColor = e.target.value;
+        },
+        stateClicked(e){
+            this.currentState = e.target.value;
+        },    
+        fueClicked(e){
+            this.currentFue = e.target.value;
+        },       
+        vitesClicked(e){
+            this.currentVites = e.target.value;
+        },      
+        hacimClicked(e){
+            this.currentMotorHacmi = e.target.value;
+        },
+        place() {            
             let data =JSON.stringify({"car_id":this.currentCar,"gallery_id":this.galleryId,"price":this.price});    
             const url = 'http://127.0.0.1:5000/placeCar'; 
 
@@ -134,8 +301,46 @@ export default {
                 console.error(error);
             });
             location.reload();            
-        }
-    }
+        },
+        enter() {
+            if (this.brandId == 0) {
+                alert('brand and model should be selected!');
+                return;
+            }
+            let data =JSON.stringify(
+                {
+                    "brand_id":this.brandId,
+                    "model_id":this.modelId,
+                    "color":this.currentColor,
+                    "state":this.currentState,
+                    "fue":this.currentFue,
+                    "vites":this.currentVites,
+                    "motor_hacmi":this.currentMotorHacmi,
+                    "motor_gucu":this.hp,
+                    "km":this.km,
+                    "gallery_id":this.galleryId,
+                    "uretim_yili":this.uretim_yili
+                });
+
+            const url = 'http://127.0.0.1:5000/enterCar'; 
+
+            fetch(url, {
+                method: 'post',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: data
+            }).then(function(response){
+                return response.json(myvalue);
+            }).then(function(text){
+                console.log(text);
+            }).catch(function (error){
+                console.error(error);
+            });
+            location.reload();  
+        }        
+    },
+
 }
 
 </script>
